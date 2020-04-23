2bd6aeb825973b79ef6fe73f1e2be187fb83d3dc
Aaron Lichtman <aaronlichtman@gmail.com>
2020-04-22 19:12:13 -0500

    Fix deadbolt cask

--- deadbolt.rb
+++ deadbolt.rb
@@ -2,7 +2,8 @@ cask 'deadbolt' do
   version '0.1.0'
   sha256 'c302f1c532082b200d844d17cd8feadc61821d97591fb5a0dbb1bb97c3a5981b'
 
-  url 'https://github.com/alichtman/deadbolt/releases/download/0.1.0/Deadbolt-0.1.0.dmg'
+  url "https://github.com/alichtman/deadbolt/releases/download/#{version}/Deadbolt-#{version}.dmg"
+  appcast 'https://github.com/alichtman/deadbolt/releases.deadbolt'
   name 'Deadbolt'
   homepage 'https://github.com/alichtman/deadbolt'
 
