dc190ec22c54cfc8f895017ef553fd63a15bab2d
Aaron Lichtman <aaronlichtman@gmail.com>
2020-04-22 19:51:34 -0500

    Fix deadbolt cask

--- Casks/deadbolt.rb
+++ Casks/deadbolt.rb
@@ -3,10 +3,9 @@ cask 'deadbolt' do
   sha256 'c302f1c532082b200d844d17cd8feadc61821d97591fb5a0dbb1bb97c3a5981b'
 
   url "https://github.com/alichtman/deadbolt/releases/download/#{version}/Deadbolt-#{version}.dmg"
-  appcast 'https://github.com/alichtman/deadbolt/releases.deadbolt'
+  appcast 'https://github.com/alichtman/deadbolt/releases.atom'
   name 'Deadbolt'
   homepage 'https://github.com/alichtman/deadbolt'
 
   app 'Deadbolt.app'
 end
-# https://github.com/Homebrew/homebrew-cask/blob/master/doc/development/adding_a_cask.md
