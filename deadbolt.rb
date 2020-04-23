e10f805e119a0a965a00b2e4b170d35df685bd65
Aaron Lichtman <aaronlichtman@gmail.com>
2020-04-22 19:01:56 -0500

    Add Deadbolt cask

--- deadbolt.rb
+++ deadbolt.rb
@@ -1,28 +1,11 @@
-class Deadbolt < Formula
-  desc "Encryption -- so simple your mom can do it."
-  homepage "https://github.com/alichtman/deadbolt"
-  # url "https://github.com/alichtman/deadbolt/releases/download/v2.0.0/gitbatch_0.5.0_darwin_amd64.tar.gz"
-  version "2.0.0"
-  sha256 ""
+cask 'deadbolt' do
+  version '0.1.0'
+  sha256 'c302f1c532082b200d844d17cd8feadc61821d97591fb5a0dbb1bb97c3a5981b'
 
-  if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/alichtman/deadbolt/releases/download/v0.1.0/_0.5.0_linux_amd64.tar.gz"
-    sha256 "d8bfaa70b128cc3e682c98dd2b127eefe4dc6a52113d5b2531117d8a4f258337"
-  end
+  url 'https://github.com/alichtman/deadbolt/releases/download/0.1.0/Deadbolt-0.1.0.dmg'
+  name 'Deadbolt'
+  homepage 'https://github.com/alichtman/deadbolt'
 
-  if OS.windows
-
-  end
-
-  if OS.mac
-
-  end
-
-  def install
-    bin.install "deadbolt"
-  end
-
-  test do
-    assert_match "deadbolt version 2.0.0", shell_output("#{bin}/gitbatch --version", 2)
-  end
+  app 'Deadbolt.app'
 end
+# https://github.com/Homebrew/homebrew-cask/blob/master/doc/development/adding_a_cask.md
